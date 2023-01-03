c3606a9285a4b44495689581666d83e332a20dbb
Alex <alexhuhaiping@gmail.com>
2023-01-03 17:40:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='pyjsondb',
     version='1.0.0',
+    description='searchable json database, easy to add,update,delete and search in any level nested json tree',
     packages=['pyjsondb', 'pyjsondb.utils'],
     url='https://github.com/alex-testlab/pyjsondb',
     license='MIT',
     author='jarbasAI,Alex',
     author_email='',
     install_requires=["pyxdg", "fasteners"],
-    description='searchable json database, easy to add,update,delete and search in any level nested json tree'
 )
