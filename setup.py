96343384919f232d446f2ba928d72f47b902b59e
Alex <alexhuhaiping@gmail.com>
2023-01-03 17:44:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='pyjsondb',
     version='1.0.0',
     description='searchable json database, easy to add,update,delete and search in any level nested json tree',
+    long_description_content_type = 'text/markdown',
     packages=['pyjsondb', 'pyjsondb.utils'],
     url='https://github.com/alex-testlab/pyjsondb',
     license='MIT',
